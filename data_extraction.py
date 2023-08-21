2bb8d9d44db2a7de4340a5287856dcaf992325a8
Israel Cohen <josh50170@gmail.com>
2023-08-21 10:35:45 +0300

    Extract data without pnctuation from files

---
+++ data_extraction.py
@@ -0,0 +1,42 @@
+import os
+import string
+
+words_dict = {}
+
+
+def remove_punctuation(input_string: str) -> str:
+    """
+    Removes punctuation from a string
+    :param input_string: The string to remove punctuation from
+    :return: The string without punctuation
+    """
+    translator = str.maketrans('', '', string.punctuation)
+    return input_string.translate(translator)
+
+
+def extract_data(path: str) -> None:
+    """
+    Extracts data from a files in nested directories to a database (dictionary)
+    :param path: The path to the file or directory
+    """
+    if os.path.isfile(path):
+        with open(path, 'r', errors='ignore') as data_file:
+            for line_num, line in enumerate(data_file):
+                line = remove_punctuation(line)
+                words = line.upper().split()
+                for word_num, word in enumerate(words):
+                    if word in words_dict:
+                        if (path, line_num) in words_dict[word]:
+                            words_dict[word][(path, line_num)].append(word_num)
+                        else:
+                            words_dict[word][(path, line_num)] = [word_num]
+                    else:
+                        words_dict[word] = {(path, line_num): [word_num]}
+    elif os.path.isdir(path):
+        for entry in os.listdir(path):
+            extract_data(os.path.join(path, entry))
+
+
+if __name__ == '__main__':
+    extract_data(r'C:\Users\josh5\Excelenteem\Python_Works_1.0\google_project\Archive')
+    print(*list(zip(words_dict.keys(), words_dict.values())), sep='\n')
